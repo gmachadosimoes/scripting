2d95fda43492a58e1958a7c630c65a58f9a58836
Gabriel Simoes <gmachadosimoes@gmail.com>
2021-04-16 20:06:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#import requests
+
+
+# url = 'https://jsonplaceholder.typicode.com/photos'
+
+
+# response = requests.get(url)
+
+
+# json_data = response.json()
+
+
+# url_list = []
+# for photo in json_data:
+  # url_list.append(photo["url"])
+
+
+# duplicates_amount = len(url_list) - len(set(url_list))
+
+
+# print(duplicates_amount)
+
